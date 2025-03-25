7afdff7e004c23368bdf01a68d6ebbf3efdd4d97
Hchan2 <seongchan_@naver.com>
2025-03-25 15:44:25 +0900

    feat: 유저 로또 입력 숫자

---
+++ src/User.js
@@ -0,0 +1,48 @@
+import { BUY_MESSAGE } from "./constant/message";
+import OPTIONS from "./constant/option";
+import sortedLottoNumbers from "./generator/make-random-number";
+import OutputView from "./view/output-view";
+
+class User {
+    constructor() {
+        this.userBuyMoney = 0;
+        this.lottoArray = [];
+        this.bonusNumber = 0;
+    }
+
+    lottoBuy(money) {
+        this.userBuyMoney = Number(money);
+        this.makeLotto();
+    }
+
+    amountOfBuying() {
+        const amount = this.userBuyMoney / OPTIONS.thousand;
+        return amount;
+    }
+
+    makeLotto() {
+        const amount = this.amountOfBuying();
+        for (let i = 0; i < amount; i++) {
+            const randomLottoNumber = sortedLottoNumbers();
+            this.saveLotto(randomLottoNumber);
+        }
+        this.printRandomNumber(this.lottoArray, amount);
+    }
+
+    saveLotto(randomNumber) {
+        this.lottoArray.push(randomNumber);
+    }
+
+    printRandomNumber(randomNumbers, amount) {
+        OutputView.printBuyAmount(BUY_MESSAGE(amount));
+        randomNumbers.forEach((numbers) => {
+            OutputView.printRandomNumbersJoin(numbers);
+        })
+    }
+
+    saveBonusNumber(bonusNumber) {
+        this.bonusNumber = Number(bonusNumber);
+    }
+}
+
+export default User;
